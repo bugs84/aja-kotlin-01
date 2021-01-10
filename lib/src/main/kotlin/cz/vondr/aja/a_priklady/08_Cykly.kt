cbb3c0e1b34dac6314107637a2b0f5effe1afa80
j.vondrous <bugsmail@seznam.cz>
2021-01-10 12:44:32 +0100

    Cyklus

---
+++ lib/src/main/kotlin/cz/vondr/aja/a_priklady/08_Cykly.kt
@@ -0,0 +1,9 @@
+package cz.vondr.aja.a_priklady
+
+fun main() {
+    var cislo = 1
+    while (cislo <= 10) {
+        println(cislo)
+        cislo = cislo + 1
+    }
+}
---
+++ lib/src/main/kotlin/cz/vondr/aja/pokusy/A10.kt
@@ -0,0 +1,7 @@
+package cz.vondr.aja.pokusy
+
+fun main() {
+     println("Jí had myši?")
+    println("za A = ano, za B = ne")
+    var odpoved = readLine()
+}
